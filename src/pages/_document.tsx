874224149c35815e558d29bd669f9bd3fd6efee2
Luiz <doginuwu@gmail.com>
2021-06-05 20:46:40 -0300

    add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,21 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <meta property="og:title" content="Julinha" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://julinha-website.vercel.app/"
+          />
+          <meta
+            property="og:image"
+            content="https://cdn.discordapp.com/icons/839112016542040064/6a3acc7f8ba7e85faf52c5629911e55e.png"
+          />
+          <meta
+            property="og:description"
+            content="Um simples bot 100% gratuito do Discord."
+          />
+          <meta name="theme-color" content="#FF0000" />
         </Head>
         <body>
           <Main />
